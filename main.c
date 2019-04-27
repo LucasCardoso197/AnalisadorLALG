91db789f2dc1a1f4a6e71ee69201813873170c31
Lucas Cardoso <lucas.sfcardoso@gmail.com>
2019-04-27 15:10:44 -0300

    Mudança na saída e impressao de erros

--- main.c
+++ main.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 #include "lexer.h"
 
 int main(){
-    int result;
+	int result;
+	char type[30];
 
-    while((result = yylex()) != 0){
-        printf("(\"%s\", %s)\n", yytext, getTypeName(result));
-    }
-    return 0;
+	while((result = yylex()) != 0){
+		if(result != identificador)
+			strcpy(type, getTypeName(result));
+		else{
+			if(yyleng > 30)
+				printf("%s - erro - identificador muito grande");
+			else {
+				// Busca na hash
+				// Se não encontrar fazer getTypeName(IDENTIFICADOR)
+				// Se encontrar faz strcpy(type, yytext)
+				if(0)
+					strcpy(type, yytext);
+				else
+					strcpy(type, getTypeName(identificador));
+			}
+		}
+		printf("%s - %s\n", yytext, type);
+	}
+	return 0;
 }
